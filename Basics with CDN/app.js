964ea7b501b2d45c0f85244d5ba16ac986e3ba4f
AMAN <antilaman3113@gmail.com>
2022-12-12 21:42:29 +0530

    first commit

---
+++ Basics with CDN/app.js
@@ -0,0 +1,9 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const element = (
+  <div>
+    <h1>Hii</h1>
+    <p>MY NAME IS AMAN</p>
+  </div>
+);
+console.log(element);
+root.render(element);
