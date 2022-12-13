a962d908b1d89bfc45f9aea6c5530baf567d1d0c
AMAN <antilaman3113@gmail.com>
2022-12-13 23:53:22 +0530

    added image to cards

--- Air Bnb Project/Components/Card.js
+++ Air Bnb Project/Components/Card.js
@@ -5,7 +5,11 @@ export default function Card(props) {
   console.log(image);
   return (
     <div className="card">
-      <img src="" alt="card-image" className="card--image" />
+      <img
+        src={require("../Src/images/wedding-photography.png")}
+        alt="card-image"
+        className="card--image"
+      />
       <div className="card--stats">
         <img
           src={require("../Src/images/Star.png")}
