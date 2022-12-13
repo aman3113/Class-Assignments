f7938dd12026c1ae6ce546c471e4d53f007bc1a6
AMAN <antilaman3113@gmail.com>
2022-12-13 10:03:12 +0530

    Navbar and hero component added

---
+++ Air Bnb Project/Components/Navbar.js
@@ -0,0 +1,10 @@
+import Logo from "../images/airbnb.png";
+import "../Src/index.css";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <img src={Logo} alt="Air Bnb Logo" className="nav--logo" />
+    </nav>
+  );
+}
---
+++ Air Bnb Project/Src/App.js
@@ -0,0 +1,10 @@
+import Navbar from "../Components/Navbar.js";
+import Hero from "../Components/Hero.js";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
